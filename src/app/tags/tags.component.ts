32719b4908d5c075435c64d9af9449a408544ce0
Danward Bautista <danwarddeveloper@gmail.com>
2023-08-25 14:12:09 +0800

    Table changes on Tags

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.scss']
 })
-export class TagsComponent {
+export class TagsComponent implements AfterViewInit {
+  displayedColumns: string[] = ['name', 'color', 'description', 'action'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
+  constructor(private modalService: NgbModal) { }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  open(content: any) {
+    this.modalService.open(content);
+
+    console.log('click');
+  }
+
+  openSm(content: any) {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  openLg(content: any) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openXl(content: any) {
+    this.modalService.open(content, { size: 'xl' });
+  }
 }
+
+export interface PeriodicElement {
+  name: string;
+  color:string;
+  description: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    { name: "Moderator", color:"#eb4034", description: "This is an example tag"},
+    { name: "Warrior", color:"#403875", description: "This is an example tag"},
+    { name: "Archer", color:"#429120", description: "This is an example tag"},
+];
