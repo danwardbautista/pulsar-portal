68d0dbd1daa534dbcbc3c55fedbb288144bd7910
Danward Bautista <danwarddeveloper@gmail.com>
2023-08-29 14:55:16 +0800

    Updates

--- src/app/action-log/action-log.component.ts
+++ src/app/action-log/action-log.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-action-log',
@@ -6,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./action-log.component.scss']
 })
 export class ActionLogComponent {
+  displayedColumns: string[] = ['operation', 'timestamp', 'user'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
+  constructor(private modalService: NgbModal) { }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  open(content: any) {
+    this.modalService.open(content);
+
+    console.log('click');
+  }
+
+  openSm(content: any) {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  openLg(content: any) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openXl(content: any) {
+    this.modalService.open(content, { size: 'xl' });
+  }
 }
+
+export interface PeriodicElement {
+  operation: string;
+  timestamp: string;
+  user: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    { operation: "Added Domain Rule", timestamp: "Mar 16 08:12:04", user:"John Smith"},
+
+];
